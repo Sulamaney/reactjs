a764e2585cd4e9262172ddc68cdaf9dbaf790b26
Sulamaney <sulaimanolanipekun@gmail.com>
2024-05-28 10:44:22 +0100

    jsreact

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import image from './kkk.jpg';
 import mali from './jjj.jpg';
 import bobu from './ggg.jpg';
 
-import Azu from './Tayo'
 import Greeter from './Bolu'
-import Ziz from './Comp'
+
 
 
   const productDetails={
-    title:['Jeans','Trouser'],
+    title:['Jeans','Trouser','Bag'],
     image:[image, mali,bobu],
-    description:['This is a Good Jean','Good Good Good',],
-    price:[25000, 10000],
+    description:['This is a Good Jean','Buy Correct Jeans','Buy Good Jeans'],
+    price:[25000, 10000, 13000],
   }
 
 function App(){ 
@@ -23,28 +22,28 @@ function App(){
           <div className='coco col'>
             <Greeter
             name={productDetails.title[0]}
-            description={productDetails.description[0]}
-            price={productDetails.price[0]}
             image={productDetails.image[0]}
+            price={productDetails.price[0]}
+            description={productDetails.description[0]}
           />
           </div>
           
 
           <div className='coc col'>
             <Greeter
-            name={productDetails.title[0]}
-            description={productDetails.description[0]}
-            price={productDetails.price[0]}
+            name={productDetails.title[1]}
             image={productDetails.image[1]}
+            price={productDetails.price[1]}
+            description={productDetails.description[1]}
           />
           </div>
         
           <div className='coo col'>
             <Greeter
-            name={productDetails.title[0]}
-            description={productDetails.description[0]}
-            price={productDetails.price[0]}
+            name={productDetails.title[2]}
             image={productDetails.image[2]}
+            price={productDetails.price[2]}
+            description={productDetails.description[2]}
           />
           </div>
   
